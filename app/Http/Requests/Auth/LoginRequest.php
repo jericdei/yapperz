86d5ca393db03ee3121455a2aba34ed7435ef167
Jeric June Logan <loganjeric25@gmail.com>
2025-01-30 16:08:30 +0800

    cleanup breeze

--- app/Http/Requests/Auth/LoginRequest.php
+++ app/Http/Requests/Auth/LoginRequest.php
@@ -12,14 +12,6 @@ use Illuminate\Validation\ValidationException;
 class LoginRequest extends FormRequest
 {
     /**
-     * Determine if the user is authorized to make this request.
-     */
-    public function authorize(): bool
-    {
-        return true;
-    }
-
-    /**
      * Get the validation rules that apply to the request.
      *
      * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
@@ -28,7 +20,6 @@ class LoginRequest extends FormRequest
     {
         return [
             'email' => ['required', 'string', 'email'],
-            'password' => ['required', 'string'],
         ];
     }
 
@@ -41,7 +32,8 @@ class LoginRequest extends FormRequest
     {
         $this->ensureIsNotRateLimited();
 
-        if (! Auth::attempt($this->only('email', 'password'), $this->boolean('remember'))) {
+        // TODO: implemenet passwordless login
+        if (! Auth::attempt($this->only('email'))) {
             RateLimiter::hit($this->throttleKey());
 
             throw ValidationException::withMessages([
